94dfd3211b3102aa5dac5413df8428fc287341be
Kailas444 <120644293+Kailas444@users.noreply.github.com>
2026-01-05 20:22:02 +0530

    Update vector_store.py

--- rag/vector_store.py
+++ rag/vector_store.py
@@ -1,26 +1,35 @@
 import os
 from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings
-from .pdf_loader import extract_pdf_pages
+from rag.pdf_loader import extract_pdf_pages
 
-def initialize_vector_store(
+def load_vector_store(
     pdf_path: str,
-    faiss_directory: str = "./faiss_store",
-    embedding_model: str = "sentence-transformers/all-MiniLM-L6-v2",
+    faiss_dir: str = "faiss_store",
 ):
-    os.makedirs(faiss_directory, exist_ok=True)
-    embeddings = HuggingFaceEmbeddings(model_name=embedding_model)
+    os.makedirs(faiss_dir, exist_ok=True)
 
-    if os.path.exists(os.path.join(faiss_directory, "index.faiss")):
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    if os.path.exists(os.path.join(faiss_dir, "index.faiss")):
         return FAISS.load_local(
-            faiss_directory, embeddings, allow_dangerous_deserialization=True
+            faiss_dir,
+            embeddings,
+            allow_dangerous_deserialization=True,
         )
 
     docs = extract_pdf_pages(pdf_path)
-    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=100,
+    )
     chunks = splitter.split_documents(docs)
 
-    vs = FAISS.from_documents(chunks, embeddings)
-    vs.save_local(faiss_directory)
-    return vs
+    vector_store = FAISS.from_documents(chunks, embeddings)
+    vector_store.save_local(faiss_dir)
+
+    return vector_store
